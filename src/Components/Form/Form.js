21858cfbc18dd85e509865d5e30ceac64d15ddf8
Rory Magee <roryemagee@gmail.com>
2022-04-21 13:54:59 -0600

    Add search input to state

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -4,13 +4,20 @@ import React, { Component } from 'react';
 class Form extends Component {
   constructor() {
     super()
-    this.state = {}
+    this.state = {
+      search: '' 
+    }
+  }
+
+  updateSearch = (e) => {
+    const {name, value} = e.target;
+    this.setState({ [name]: value });
   }
   
   render() {
     return (
       <div className="search">
-        <input type="text" />
+        <input className="search-bar" type="text" placeholder="Search" value={this.state.search} name="search" onChange={(e) => this.updateSearch(e)}/>
       </div>
     );
   }
