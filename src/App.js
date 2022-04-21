60b153fa12d8cd0b0ba98bd328bfac97f269aceb
Rory Magee <roryemagee@gmail.com>
2022-04-20 22:01:00 -0600

    Add background images

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import easelandcanvas from './Assets/easelandcanvas.png';
+import artExhibit from './Assets/ArtExhibit.jpeg'
 import React, { Component } from 'react';
 
 class App extends Component {
@@ -10,11 +11,10 @@ class App extends Component {
   
   render() {
     return (
-      <div className="background">
-        <h1 className="placeholder"> Myseum</h1>
-        <div className="easel-container">
-          <img src={easelandcanvas} className="easel" />
-        </div>
+      <div className="everything">
+        <h1 className="title"> Myseum</h1>
+        <img src={artExhibit} className="background" />
+        <img src={easelandcanvas} className="easel" />
       </div>
     );
   }
