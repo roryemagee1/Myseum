031efdcf21c2a0f8fd84532fdbbbca161d49db8a
Rory Magee <roryemagee@gmail.com>
2022-04-20 19:58:31 -0600

    Refactor App into class component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {}
+  }
+  
+  render() {
+    return (
+      <div>
+        <h1> Test </h1>
+      </div>
+    );
+  }
 }
 
 export default App;
