fd53ab790f84fd63a13759719bfa9c18d107027b
Rory Magee <roryemagee@gmail.com>
2022-04-20 21:21:33 -0600

    Add apiCalls file

---
+++ src/apiCalls.js
@@ -0,0 +1,12 @@
+const apiCalls = {
+
+}
+
+// https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&q=sunflowers
+
+// https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&medium=Paintings&q=sunflowers
+
+
+
+// https://collectionapi.metmuseum.org/public/collection/v1/objects/[objectID]
+
