f6e5e5dce064be6cb42b5f5131389c2d3bd59063
Rory Magee <roryemagee@gmail.com>
2022-04-21 15:42:31 -0600

    Add painting fetch call

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,6 +2,11 @@ const apiCalls = {
   fetchPaintingIDs(search) {
     return fetch(`https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&medium=Paintings&q=${search}`)
       .then(response => response.json())
+  },
+
+  fetchPainting(id) {
+    return fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${id}`)
+      .then(response => response.json())
   }
 }
 
@@ -10,8 +15,6 @@ export default apiCalls;
 // https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&q=sunflowers
 
 // https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&medium=Paintings&q=sunflowers
-
-
 
 // https://collectionapi.metmuseum.org/public/collection/v1/objects/[objectID]
 
