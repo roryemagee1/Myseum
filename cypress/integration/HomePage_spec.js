8d296c5f045e38c651fc48cb777708cf90e6ca27
Rory Magee <roryemagee@gmail.com>
2022-04-24 17:59:38 -0600

    Add base url test

--- cypress/integration/HomePage_spec.js
+++ cypress/integration/HomePage_spec.js
@@ -5,7 +5,10 @@ beforeEach(() => {
 })
 
 describe('Basic Tests', () => {
+  it('should display the home url when the webpage loads', () => {
+    cy.url().should('eq', 'http://localhost:3000/');
+  })
   it('Should contain a blank canvas image', () => {
-    
+    cy.get()
   })
 })
