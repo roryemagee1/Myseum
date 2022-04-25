13f391931dd0e8627ad76812b9e89ad7454e8d2d
Rory Magee <roryemagee@gmail.com>
2022-04-24 18:12:44 -0600

    Consolidate basic homepage display tests into one test

--- cypress/integration/HomePage_spec.js
+++ cypress/integration/HomePage_spec.js
@@ -4,11 +4,37 @@ beforeEach(() => {
   cy.visit('http://localhost:3000/')
 })
 
-describe('Basic Tests', () => {
+describe('HomePage Tests', () => {
   it('should display the home url when the webpage loads', () => {
     cy.url().should('eq', 'http://localhost:3000/');
   })
+
+  it('Should contain an art exhibit background image, a blank canvas, and a Myseum header', () => {
+    cy.get('.background')
+      .should('have.attr', 'src')
+      .and('contain', '/static/media/ArtExhibit.73a73c5419becb2a9d24.jpeg')
+
+    cy.get('.easel')
+    .should('have.attr', 'src')
+    .and('contain', '/static/media/easelandcanvas.b7ffcfe6e6fd17ea82e1.png')
+
+    cy.get('h1')
+      .contains('Myseum')
+  })
+
   it('Should contain a blank canvas image', () => {
-    cy.get()
+    cy.get('.easel')
+      .should('have.attr', 'src')
+      .and('contain', '/static/media/easelandcanvas.b7ffcfe6e6fd17ea82e1.png')
+  })
+
+  it('Should contain a Myseum header', () => {
+    cy.get('h1')
+      .contains('Myseum')
+  })
+
+  it('Should contain an empty search input', () => {
+    cy.get('input')
+      .should('have.attr', 'src')
   })
 })
